0e53a5a59cccd755d97cf30575b5cd3e4becd5f5
StepanZapotichnyi <125075465+StepanZapotichnyi@users.noreply.github.com>
2023-10-04 20:29:51 +0200

    Created search method APEX and JS

--- force-app/main/default/lwc/note/note.js
+++ force-app/main/default/lwc/note/note.js
@@ -1,93 +1,76 @@
-import { LightningElement, track, wire} from 'lwc';
+
+import { LightningElement, track } from 'lwc';
 import { ShowToastEvent} from 'lightning/platformShowToastEvent';
 import MyModal from 'c/myModal';
 import saveData from '@salesforce/apex/NoteController.saveData'
 import deleteNoteOnServer from '@salesforce/apex/NoteController.deleteNoteOnServer';
 import getSavedNotes from '@salesforce/apex/NoteController.getSavedNotes';
-
+import searchNotesByDate from '@salesforce/apex/NoteController.searchNotesByDate';
 export default class Note extends LightningElement {
 
     @track savedData= [];
+    @track searchDate = '';
 
-
-    //1
-    @wire(getSavedNotes)
-    wiredSavedNotes({ error, data}){
-        console.log('Wire is called');
-        if (data) {
-            this.savedData = data;
-            console.log("Result: " + data);
-        } else if (error) {
-            console.error('Error loading data', error);
-        }
+    handleDateChange(event) {
+        this.searchDate = event.target.value;
     }
-    // connectedCallback(){
 
-    //     const savedData = localStorage.getItem('savedData'); //retrive from database
-    //     if (savedData) {
-    //         this.savedData = JSON.parse(savedData);
-    //     }
-    // }
-    
 
+    loadSavedData() {
+         getSavedNotes()
+            .then((result)=>{
+                this.savedData = result;
+            })
+            .catch((error)=> {
+            console.error('Error loading data', error);
+            });
+    }
+    connectedCallback(){
+        this.loadSavedData();
+    }
+
+    //to do does not update when added
     async handleAdd(){
         const result = await MyModal.open({
             size: 'Small',
             label: 'Note',
             description: 'This is a modal popup'
         });
-        
-        // this.result = result;
-        // console.log('this.result = ' + this.result);  
-         
-    
-        if (result) {
 
-            console.log('Result 2: ' + result);
-            saveData({
-                label: result.label,
-                description: result.description
-            })
-            .then((savedNoteId)=> {
-                if(savedNoteId) {
-                    
-                    // getSavedNotes()
-                    //     .then((result)=>{
-                    //         this.savedData = result;
-                    //     })
-                    //     .catch((error) => {
-                    //         console.error('Error fetching saved data', error);
-                    //     });
-                    this.savedData = [...this.savedData,{
-                        Id: savedNoteId,
-                        Label__c: result.label,
-                        Description__c: result.description,
-                        Created_Date__c: new Date().toISOString()
-                    }];
+        if(result) {
+            try{
+                const savedNoteId = await saveData({
+                    label: result.label,
+                    description: result.description
+                });
+                    if(savedNoteId){
+                        const savedNote = {
+                            Id: savedNoteId,
+                            Label__c: result.label,
+                            Description__c: result.description,
+                            Created_Date__c: new Date().toISOString()
+                        };
+                        this.savedData = [...this.savedData, savedNote];
 
-                    // result.Id = savedNote;
-                    // this.savedData = [...this.savedData, savedNote]
-                    this.dispatchEvent(
-                        new ShowToastEvent({
-                            title: 'Success',
-                            message: 'Data saved successfully',
-                            variant: 'success'
-                        })
-                    );
+                        this.dispatchEvent(
+                            new ShowToastEvent({
+                                title: 'Success',
+                                message: 'Data saved successfully',
+                                variant: 'success',
+                            })
+                        );
+                        this.loadSavedData();
+                    }else {
+                        this.dispatchEvent(
+                            new ShowToastEvent({
+                                title: 'Error',
+                                message: 'An error occurred while saving data',
+                                variant: 'error'
+                            })
+                        );
+                    }
             
-                }else{
-                    this.dispatchEvent(
-                        new ShowToastEvent({
-                            title: 'Error',
-                            message: 'An error occurred while saving data',
-                            variant: 'error'
-                        })
-                    );
-                }
-                
-                // this.result = result;
-            })
-            .catch(error => {
+                } catch (error)  {
                 console.error('Error saving data: ',error)
                 this.dispatchEvent(
                     new ShowToastEvent({
@@ -96,52 +79,56 @@ export default class Note extends LightningElement {
                         variant: 'error'
                     })
                 );
-            });
+            }
         }
     }
 
     
-    handleDelete(event) {
+    async handleDelete(event) {
         const noteId = event.target.dataset.id;
         // console.log(event?.detail?.dataset?.id);
     
-    
-        deleteNoteOnServer({noteId: noteId})
-            .then((result) => {
-                if (result === "Success") {
-                    //
-                    // getSavedNotes()
-                    //     .then((data)=>{
-                    //         this.savedData = data;
-                    //     })
-                    //     .catch((error) => {
-                    //         console.error('Error fetching saved data', error);
-                    //     });
-                    this.savedData = this.savedData.filter((note) => note.Id !== noteId);
-                    this.dispatchEvent(
-                        new ShowToastEvent({
-                            title: 'Success',
-                            message: 'Data deleted successfully',
-                            variant: 'success'
-                        })
-                    );
-                    
-                } else {
-                    console.error('Error deleting note: ', result);
-                    this.dispatchEvent(
-                        new ShowToastEvent({
-                            title: 'Error',
-                            message: 'An Error occurred while deleting data: ' + result,
-                            variant: 'error'
-                        })
-                    );
-                }
-            })
-            .catch((error) => {
-                console.error('Error deleting note:', error);
+        try {
+            const result = await deleteNoteOnServer({ noteId: noteId });
 
-            });
+            if (result === 'Success'){
+                this.savedData = this.savedData.filter((note)=> note.Id !== noteId);
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'Data deleted successfully',
+                        variant: 'success'
+                    })
+                );
+                // this.loadSavedData();
+            }else {
+                console.error('Error deleting note: ', result);
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error',
+                        message: 'An Error occurred while deleting data: ',
+                        variant: 'error'
+                    })
+                );
+            }
+        }catch(error){
+            console.error('Error deleting note: ', error);
+        }
     }
-
-
+// looking for today's date only ????
+    async handleSearch(){
+        try{
+            this.savedData = await searchNotesByDate({ searchDate: this.searchDate });
+            console.log('Res ' +  this.savedData);
+        }catch(error){
+            console.error('Error searching notes by dae: ', error);
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error',
+                    message: 'An error occurred while searching for notes',
+                    variant: 'error'
+                })
+            );
+        }
+    }
 }
